862086e12edb391e5f075c8d4584e8950eaa20cf
Andrea Orlando <71812411+andreaorlando333@users.noreply.github.com>
2021-03-17 08:43:42 +0100

    Add files via upload

---
+++ Java/esercizio_toast/app/src/main/java/com/example/esercizio_toast/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.esercizio_toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnMostra;
+    private EditText txtInput;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnMostra = (Button)findViewById(R.id.btnMostra);
+        txtInput = (EditText) findViewById(R.id.txtInput);
+
+        btnMostra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messaggio = txtInput.getText().toString();
+                Toast.makeText(MainActivity.this, messaggio, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
