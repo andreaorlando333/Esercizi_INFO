3f7d50c4738479bc369c0a4c8fd9810f8b390b57
Andrea Orlando <71812411+andreaorlando333@users.noreply.github.com>
2021-05-04 12:42:13 +0200

    Add files via upload

---
+++ Javascript/arrayAssociativo/array.js
@@ -0,0 +1,21 @@
+
+// Array associativo
+
+function funzione(){
+
+	//...
+	return 43;
+}
+
+var foo = function() {
+	//...
+	return 42;
+}
+
+var persona = {};
+persona["nome"] = "Andrea";
+persona["cognome"] = "Orlando";
+
+alert(persona["nome"]); // Stampa campo "Nome"
+alert(persona.cognome); // altro modo per stampare
+
